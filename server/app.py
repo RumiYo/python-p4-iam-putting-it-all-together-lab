ddb1b16b762334e957777fbc5db095a7e7a4a47d
RumiYo <yokoyamarumi@gmail.com>
2024-07-13 15:31:11 -0400

    Complete lab

--- server/app.py
+++ server/app.py
@@ -8,19 +8,87 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+
+    def post(self):
+        json_data = request.get_json()
+        if not json_data.get('username'):
+            return {"message": "Username cannot be empty"}, 422
+        duplicate_name_user = User.query.filter(User.username==json_data['username']).first()
+        if duplicate_name_user:
+            return {"message": "This username is already used. Try with a new username"}, 422
+        new_record = User(
+            username = json_data.get('username'),
+            image_url = json_data.get('image_url'),
+            bio = json_data.get('bio')
+        )
+
+        new_record.password_hash = json_data.get('password')
+
+        db.session.add(new_record)
+        db.session.commit()
+        print(new_record)
+
+        session['user_id'] = new_record.id
+        response = new_record.to_dict()
+        print(response)
+
+        return new_record.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id==session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {'mesage': '401: Not Authorized'}, 401 
 
 class Login(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+        user = User.query.filter(User.username==json_data.get('username')).first()
+        if user and user.authenticate(json_data.get('password')):
+            session['user_id'] = user.id
+            return user.to_dict()
+
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if not session['user_id']:
+            return {'error': 'You are not logged in'}, 401
+
+        session['user_id']= None
+        return {}, 204
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if not session['user_id']:
+            return {'message': 'Please signup/login to see Recipe'}, 401
+        recipes = Recipe.query.all()
+        recipes_dict = []
+        for recipe in recipes:
+            recipes_dict.append(recipe.to_dict())
+        return recipes_dict, 200  
+
+    def post(self):
+        if not session['user_id']:
+            return {'message': 'Please signup/login to add recipes'}, 401
+        json_data = request.get_json()
+        if len(json_data.get('instructions'))<=50:
+            return {'error': 'Instruction must be at least 50 characters long'}, 422
+        new_record = Recipe(
+            title =json_data.get('title'),
+            instructions=json_data.get('instructions'),
+            minutes_to_complete=json_data.get('minutes_to_complete'),
+            user_id=session.get('user_id')
+        )
+        db.session.add(new_record)
+        db.session.commit()
+        if new_record.id:        
+            return new_record.to_dict(), 201
+        return {'error': 'The input information is not valid'}, 422
+           
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -6,10 +6,76 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    serialize_rules = ('-recipes.user')
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates="user")
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hash is not accessible')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "username": self.username,
+            "image_url": self.image_url,
+            "bio": self.bio
+        }
+    
+    def __repr__(self):
+        return f"<User {self.id}, {self.username}, {self.image_url}, {self.bio}>"
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    serialize_rules = ('-user.recipes')
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String,nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # __table_args__ = (
+    #     CheckConstraint('length(instructions) >=50'),
+    # )
+
+    user = db.relationship('User', back_populates="recipes")
+
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if len(instructions) <= 50: 
+            raise ValueError("Instruction must be at least 50 characters long")
+        return instructions
+
+    def to_dict(self):
+        return {      
+            "title": self.title,
+            "instructions": self.instructions,
+            "minutes_to_complete": self.minutes_to_complete,
+            "user":{
+                "user_id": self.user.id,
+                "username": self.user.username,
+                "image_url": self.user.image_url,
+                "bio": self.user.bio
+            }
+        }
